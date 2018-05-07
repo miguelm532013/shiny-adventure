4aaa65c122a439bc2b83f92087cf8f14d62c61f6
Anirudh <33914197+co-dex9507@users.noreply.github.com>
2018-05-07 02:39:27 -0500

    Initial commit. Add basic scaffolding for front end.

---
+++ shiny-frontend/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Story.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Write your story, one word at a time!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
